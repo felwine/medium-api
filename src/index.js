e8b75f38c81e060c11e73424b4da676e7c037230
Aboubacar Doucoure <yelounak@gmail.com>
2024-05-02 19:14:51 +0200

    feat: first commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,1 @@
-
-export * as post from './post/index.js'
-export * as platform from './platform/index.js'
-export * as project from './project/index.js'
+export { MediumClient, MediumError } from './medium.js'
--- src/lib/remark/mdastToHTML.js
+++
@@ -1,8 +0,0 @@
-import { toHast } from 'mdast-util-to-hast'
-import { toHtml } from 'hast-util-to-html'
-
-export default ({
-  mdast,
-} = {}) => {
-  return toHtml(toHast(mdast))
-}
